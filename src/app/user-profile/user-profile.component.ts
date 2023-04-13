f8067df00e7cbbbaf28a49282700506718ec6205
Alexander Zhidkov <azhidkov@gmail.com>
2023-04-13 16:59:41 +0800

    edit user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth, user } from '@angular/fire/auth';
+import { DocumentData, DocumentReference, Firestore, doc, getDoc, updateDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+  private firestore: Firestore = inject(Firestore);
+  private auth: Auth = inject(Auth);
+  user$ = user(this.auth);
+  user: any;
+  isLoading: boolean = true;
+  userRef: DocumentReference<DocumentData> | undefined = undefined;
 
+  constructor() {
+    this.user$.subscribe((user) => {
+      this.userRef = doc(this.firestore, `users/${user?.uid}`);
+      getDoc(this.userRef).then((user) => {
+        this.user = user.data();
+        this.isLoading = false;
+      });
+    });
+  }
+
+  updateUser(partialUser: any) {
+    if (this.userRef) {
+      updateDoc(this.userRef, partialUser);
+    }
+  }
 }
