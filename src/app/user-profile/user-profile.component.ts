b6e3d3c194a1b6f16dcc4ed8b993de15990d4ee1
Alexander Zhidkov <azhidkov@gmail.com>
2023-05-24 15:58:37 +0800

    update profile image and store in firebase

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { DocumentData, DocumentReference, Firestore, doc, getDoc, updateDoc } from '@angular/fire/firestore';
-import { Storage, ref, uploadBytesResumable, getDownloadURL } from '@angular/fire/storage';
+import { Storage, ref, uploadString, getDownloadURL } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-user-profile',
@@ -23,8 +23,7 @@ export class UserProfileComponent implements OnInit {
     this.userRef = doc(this.firestore, `users/${authUser?.uid}`);
     this.user = (await getDoc(this.userRef)).data();
     this.profileImageUrl = this.user.isProfileImageInStorage ?
-      await getDownloadURL(ref(this.storage, this.user?.uid)) :
-      this.user.photoURL;
+      await getDownloadURL(ref(this.storage, `profile-images/${this.user?.uid}-240`)) : this.user.photoURL;
     this.isLoading = false;
   }
 
@@ -34,58 +33,90 @@ export class UserProfileComponent implements OnInit {
     }
   }
 
-  onImageSelected(event: any) {
-    const file = event.target.files[0];
-    const storageRef = ref(this.storage, this.user?.uid);
+  async onImageSelected(event: Event) {
     this.isImageLoading = true;
-    uploadBytesResumable(storageRef, file).then(async () => {
-      this.updateUser({ isProfileImageInStorage: true });
-      this.profileImageUrl = await getDownloadURL(storageRef);
-      this.isImageLoading = false;
+    const files = (event.target as HTMLInputElement).files;
+    if (!files || files.length === 0) return;
+    const file = files[0];
+
+    //Extension is not used because it's not stored in the database for image retrieval.
+    //const extension = file.name.split('.')[1];
+    const img = new Image();
+    img.onload = () => {
+      var imageSize = 240;
+      const img240 = this.getResizedImageDataUrl(img, file.type, imageSize);
+      if (img240) {
+        uploadString(ref(this.storage, `profile-images/${this.user?.uid}-${imageSize}`), img240, 'data_url')
+          .then(async () => {
+            this.updateUser({ isProfileImageInStorage: true });
+            this.profileImageUrl = await getDownloadURL(ref(this.storage, `profile-images/${this.user?.uid}-240`));
+            this.isImageLoading = false;
+          })
+          .catch((error) => {
+            console.error(error);
+            this.isImageLoading = false;
+          });
+      } else {
+        this.isImageLoading = false;
+      }
+      imageSize = 50;
+      const img50 = this.getResizedImageDataUrl(img, file.type, imageSize);
+      if (img50) {
+        uploadString(ref(this.storage, `profile-images/${this.user?.uid}-${imageSize}`), img50, 'data_url')
+          .catch((error) => {
+            console.error(error);
+          });
+      };
+    }
+
+    const dataUrl = await this.readFile(file);
+    uploadString(ref(this.storage, `profile-images/${this.user?.uid}`), dataUrl, 'data_url');
+
+    img.src = dataUrl;
+  }
+
+  readFile(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = reject;
+
+      reader.readAsDataURL(file);
     });
   }
-  /* ToDo: Resize image
-  const reader = new FileReader();
-    reader.onload = (e) => {
-    if (!e.target) return;
 
-    var img = new Image();
-    img.src = e.target.result as string;
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
+  getResizedImageDataUrl(img: HTMLImageElement, fileType: string, maxSize: number) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
     if (!ctx) return;
-    ctx.drawImage(img, 0, 0);
 
-    var MAX_WIDTH = 240;
-    var MAX_HEIGHT = 240;
     var width = img.width;
     var height = img.height;
-
     if (width > height) {
-      if (width > MAX_WIDTH) {
-        height *= MAX_WIDTH / width;
-        width = MAX_WIDTH;
+      if (width > maxSize) {
+        height = height * (maxSize / width);
+        width = maxSize;
       }
     } else {
-      if (height > MAX_HEIGHT) {
-        width *= MAX_HEIGHT / height;
-        height = MAX_HEIGHT;
+      if (height > maxSize) {
+        width = width * (maxSize / height);
+        height = maxSize;
       }
     }
     canvas.width = width;
     canvas.height = height;
-    var ctx = canvas.getContext("2d");
-    if (!ctx) return;
-    ctx.drawImage(img, 0, 0, width, height);
 
-    dataurl = canvas.toDataURL(file.type);
-    document.getElementById('output').src = dataurl;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
+    const resizedDataUrl = canvas.toDataURL(fileType);
+    return resizedDataUrl;
+  }
 
-    this.updateUser({ photoURL: reader.result });
-  };
-    reader.readAsDataURL(file);
-  loadImage
-  reader
-  */
+  async openFullSizeImage() {
+    const fullSizeImageUrl = this.user.isProfileImageInStorage ?
+      await getDownloadURL(ref(this.storage, `profile-images/${this.user?.uid}`)) : this.user.photoURL;
+    window.open(fullSizeImageUrl);
+  }
 }
