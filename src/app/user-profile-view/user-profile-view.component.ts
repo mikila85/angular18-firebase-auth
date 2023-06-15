27a08a4d57e93ad77735a04c5c1c817704a9a768
Alexander Zhidkov <azhidkov@gmail.com>
2023-06-15 17:25:36 +0800

    profile image url

--- src/app/user-profile-view/user-profile-view.component.ts
+++ src/app/user-profile-view/user-profile-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { doc, Firestore, getDoc } from '@angular/fire/firestore';
+import { Storage, ref, getDownloadURL } from '@angular/fire/storage';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,7 +10,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserProfileViewComponent implements OnInit {
   private firestore: Firestore = inject(Firestore);
+  private storage: Storage = inject(Storage);
   user: any;
+  profileImageUrl: string | undefined = undefined;
   isLoading: boolean = true;
 
   constructor(
@@ -20,6 +23,8 @@ export class UserProfileViewComponent implements OnInit {
     const userId = this.route.snapshot.paramMap.get('userId');
     const docRef = doc(this.firestore, `users/${userId}`);
     this.user = (await getDoc(docRef)).data();
+    this.profileImageUrl = this.user.isProfileImageInStorage ?
+      await getDownloadURL(ref(this.storage, `profile-images/${this.user?.uid}-240`)) : this.user.photoURL;
     this.isLoading = false;
   }
 }
